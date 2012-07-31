42b1aab6496614579ca15409b44d1b890fdad3ad
Scott <scott@dottedt.com>
2012-07-31 12:09:30 -0500

    a little help

--- launcher.rb
+++ launcher.rb
@@ -13,6 +13,16 @@ class Launcher
       @app_map[ ftype ]
   end
 
+  def file_type file_name
+      File.extname( file_name ).gsub( /^\./, '' ).downcase
+  end
+
+  def help
+      print "You must pass in the path to the file to launch. Usage: #{__FILE__} target_file"
+  end
+
+
+
 
   puts "it worked"
 end
