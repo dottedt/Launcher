10b370108490278a66f16c780ef0a6d0469658b0
Scott <scott@dottedt.com>
2012-07-31 12:06:04 -0500

    file app association

--- launcher.rb
+++ launcher.rb
@@ -3,6 +3,12 @@ class Launcher
     @app_map =  app_map
   end
 
+  def run file_name
+      application = select_app file_name
+      system "#{application} #{file_name}" )
+  end
+
+
   puts "it worked"
 end
 
