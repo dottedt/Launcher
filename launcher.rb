000097b7cf17d67b904886e581550456c5e63be8
Scott <scott@dottedt.com>
2012-08-01 09:54:16 -0500

    check if ARGV is empty, set up app_map and launch.  Launching with both alias and direct call to applicaiton

--- launcher.rb
+++ launcher.rb
@@ -1,30 +1,40 @@
+#!/usr/local/bin/ruby
+
 class Launcher
-  def initialize app_map
-    @app_map =  app_map
-  end
-
-  def run file_name
-      application = select_app file_name
-      system "#{application} #{file_name}" )
-  end
-
-  def select_app file_name
-      ftype = file_type file_name
-      @app_map[ ftype ]
-  end
-
-  def file_type file_name
-      File.extname( file_name ).gsub( /^\./, '' ).downcase
-  end
-
-  def help
-      print "You must pass in the path to the file to launch. Usage: #{__FILE__} target_file"
+  def initialize( app_map )
+    @app_map = app_map
   end
 
 
+  def run( file_name )
+    application = select_app( file_name )
+    system( "#{application} #{file_name}" )
+  end
 
+  def select_app( file_name )
+    ftype = file_type( file_name )
+    @app_map[ ftype ]
+  end
 
-  puts "it worked"
+  def file_type( file_name )
+    File.extname( file_name ).gsub( /^\./, '' ).downcase
+  end
+end
+def help
+  print "You must pass in the path to the file to launch. Usage: #{__FILE__} target_file"
 end
 
-launcher = Launcher.new
+if ARGV.empty?
+  help
+  exit
+else
+  app_map = {
+    'html' => '/Applications/Firefox.app/Contents/MacOS/firefox-bin',
+    'rb' => 'subl',
+    'jpg' => '/Applications/Adobe\ Photoshop\ CS5.1/Adobe\ Photoshop\ CS5.1.app/Contents/MacOS/Adobe\ Photoshop\ CS5.1'
+  }
+  l = Launcher.new( app_map )
+  target = ARGV.join( ' ' )
+  l.run( target )
+end
+
